784aeee7a727bc627d9019102ce05b1b22e95e42
tynzer <61068743+tynzer@users.noreply.github.com>
2022-10-03 17:23:03 -0300

    Update CarritosDaoMongoDb.js
    No es valido timestamp true

--- src/daos/carritos/CarritosDaoMongoDb.js
+++ src/daos/carritos/CarritosDaoMongoDb.js
@@ -5,8 +5,7 @@ import ContenedorMongoDb from "../../contenedores/ContenedorMongoDb.js";
 export default class CarritosDaoMongoDb extends ContenedorMongoDb{
     constructor() {
         super('carrito', {
-            productos: { type: [], required: true },
-            timestamps: true
+            productos: { type: [], required: true }
         })
 
     }
