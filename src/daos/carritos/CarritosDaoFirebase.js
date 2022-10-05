6164a6790a94293a87a9f21ec39aeee42fe12f2e
aragoneloy <78914168+aragoneloy@users.noreply.github.com>
2022-10-05 08:58:58 -0300

    Revert "Arreglo "

--- src/daos/carritos/CarritosDaoFirebase.js
+++
@@ -1,7 +0,0 @@
-import ContenedorFirebase from "../../contenedores/ContenedorFirebase.js"
-
-export default class CarritosDaoFirebase extends ContenedorFirebase {
-    constructor(){
-    super('carritos')}
-}
-
--- src/daos/productos/ProductosDaoFirebase.js
+++
@@ -1,7 +0,0 @@
-import ContenedorFirebase from "../../contenedores/ContenedorFirebase.js"
-
-export default class ProductosDaoFirebase extends ContenedorFirebase {
-    constructor(){
-    super('productos')}
-}
-
